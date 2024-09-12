aa8d6ab31f1a665a37578fc4d7c4466c2a4385df
ehachero <hachero@avaya.com>
2024-09-12 11:55:49 +0200

    cambiando config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Importante para rutas relativas en Vercel
   build: {
     lib: {
-      entry: 'src/WebHelloReact.jsx', // El archivo donde defines tu Web Component
-      name: 'MyWebComponent', // Nombre de la biblioteca, accesible como variable global
-      fileName: (format) => `my-web-component.${format}.js`, // Nombre del archivo de salida
-      formats: ['umd', 'es'], // Formatos para exportar: UMD para usar en el navegador y ES para importar
+      entry: 'src/WebHelloReact.jsx', // Archivo de entrada para el Web Component
+      name: 'MyWebComponent',
+      fileName: (format) => `my-web-component.${format}.js`,
+      formats: ['umd', 'es'], // UMD para usar en el navegador
     },
     rollupOptions: {
-      // Si tienes dependencias externas (p. ej., React), aquí las defines para que no se incluyan en el bundle
       external: ['react', 'react-dom'],
       output: {
         globals: {
@@ -19,8 +19,8 @@ export default defineConfig({
         }
       }
     },
-    outDir: 'dist', // Directorio donde se generarán los archivos compilados
-    emptyOutDir: true // Limpia el directorio de salida antes de cada compilación
+    outDir: 'dist', // Directorio de salida
+    emptyOutDir: true
   },
   plugins: [react()]
 });
